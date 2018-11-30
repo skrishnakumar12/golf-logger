3cd738db91b6d6ee96d0a067362e0cee679da260
skrishnakumar12 <shankar.krish12@gmail.com>
2018-11-30 13:37:35 -0500

    Started adding a navigation bar and server code

--- README.md
+++
@@ -1,2 +0,0 @@
-# Golf-Logger
-A CS252 Project for tracking golf stats by Shankar and Zach test
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <NavBar/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            Work in progress!
+        </p>
         </header>
       </div>
     );
