72c5a13a9c3ab02c3b374a04a2e2472b9d7ce01f
skrishnakumar12 <shankar.krish12@gmail.com>
2018-12-02 22:59:12 -0500

    Updated server to be able to read from and write to the firebase database

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-//import dependencies
-const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-var mysql = require('mysql')
-// const helmet = require('helmet');
-// const morgan = require('morgan');
+var express = require('express');
+var firebase = require("firebase-admin");
+const bodyParser = require('body-parser');
+var serviceAccount = require("./serviceAccountKey.json");
 
+// Setup firebase credentials
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://golf-logger-3dce5.firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref("stats_table");
+var entriesRef = ref.child("entries");
 // define the Express app
-const app = express();
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,97 +23,51 @@ app.set('port', (process.env.PORT || 5000));
 console.log("NODE_ENV: ", process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-
+  
   // Return the main index.html, so react-router render the route in the client
   app.get('/', (req, res) => {
     res.sendFile(path.resolve('frontend/build', 'index.html'));
   });
 }
 
-
-// // enhance your app security with Helmet
-// app.use(helmet());
-
-// // use bodyParser to parse application/json content-type
-// app.use(bodyParser.json());
-
-
-// // log HTTP requests
-// app.use(morgan('combined'));
-
-//Connect to mySQL database
-const connection = mysql.createConnection({
-  //host: "localhost",  //Change this
-  //user: "root",  //Change this
-  //password: "password",       //Change this
-  //database: "zbridwel",  //Change this
-   host: "golf-logger.database.windows.net",  //Change this
-   user: "zbridwel",  //Change this
-   password: "Cs252proj",       //Change this
-   database: "golf-logger",  //Change this
-  insecureAuth: true
-})
-
-connection.connect(function(err) {
-  if(err) {
-      console.error("Error connecting: " + err.stack);
-      return;
-  }
-    console.log("Connected as id: " + connection.threadId);
-})
-
-// // enable all CORS requests
-// app.use(cors());
-
 // retrieve Basic message
 app.get('/', (req, res) =>{
-  res.send('You are now using the site')
+    res.send('You are now using the site')
 })
 
 //Retrieve all the user data
-app.get('/user', (req, res) => {
-  const name_id = req.query.username;
-  const SELECT_SPECIFIC_USER_QUERY = `SELECT * FROM golf_table where username='${name_id}';`;
-  //const SELECT_ALL_USER_QUERY = `SELECT * FROM golf_table;`;
-  connection.query(SELECT_SPECIFIC_USER_QUERY, (err, results) => {
-      if(err) {
-          console.log("Error with query: " + SELECT_ALL_USER_QUERY);
-          return res.send(err)
-      } else {
-          return res.json({
-              data: results
-          })
-      }
-  })
+app.get('/:user', (req, res) => {
+    let name = req.params.user;
+    // let course = req.query.course;
+    // let date = req.query.date;
+    // let score = req.query.score;
+    // console.log("Username: " + name + " Course name: " + course +
+    //             " Date of score: " + date + " Score: " + score);
+    //console.log("User: " + name);
+    // res.json(req.query);
+    ref.once("value", function(snapshot) {
+        console.log(snapshot.val());
+    })
 })
 
 // Insert a new score
-app.get('/user/add', (req, res) => {
-  const{ username, course_name, date_score, score } = req.query
-  const INSERT_SCORE_QUERY = `INSERT INTO golf_table(username, course_name, date_score, score) VALUES('${username}', '${course_name}', '${date_score}', '${score}');`
-  connection.query(INSERT_SCORE_QUERY, (err, results) => {
-      if(err) {
-          return res.send(err)
-      } else {
-          return res.send('Score addition was a success')
-      }
-  })
+app.get('/:user/add', (req, res) => {
+    let name = req.params.user;
+    let course = req.query.course;
+    let date = req.query.date;
+    let score = req.query.score;
+    // console.log("Username: " + name + ", Course name: " + course +
+    //             ", Date of score: " + date + ", Score: " + score);
+    // res.json(req.query);
+    entriesRef.push({
+        username: name,
+        course_name: course,
+        date_score: date,
+        score: score
+    })
 })
-
-// // insert a new entry
-// app.post('/', (req, res) => {
-//   const {userID, course, date, score} = req.body;
-//   const newEntry = {
-//     userID,
-//     course,
-//     date,
-//     score,
-//   };
-//   entries.push(newEntry);
-//   res.status(200).send();
-// });
 
 // start the server
 app.listen(app.get('port'), () => {
-  console.log(`listening on port ${app.get('port')}`);
+    console.log(`listening on port ${app.get('port')}`);
 });
